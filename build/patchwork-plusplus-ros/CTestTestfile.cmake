7d6d3080151401076b825ce8a52f83afaa08caa7
11Right-Ear <Lan7NASA666@outlook.com>
2025-12-11 17:30:14 +0800

    能运行数据包可视化的demo

--- build/patchwork-plusplus-ros/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kkcqh/dengyinghao/work/patchwork/patchwork_ws/src/patchwork-plusplus-ros
-# Build directory: /home/kkcqh/dengyinghao/work/patchwork/patchwork_ws/build/patchwork-plusplus-ros
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("include/jsk_recognition_msgs")
